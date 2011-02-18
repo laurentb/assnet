21d8fe19c4570c072efa804812c9a4e7b24db813
Romain Bignon <romain@peerfuse.org>
2011-02-18 17:51:28 +0100

    new Context object

--- ass2m/server.py
+++ ass2m/server.py
@@ -39,7 +39,7 @@ class Ass2mFileApp(FileApp):
             content_type += "; charset=UTF-8"
         return (content_type, guess[1])
 
-class Actions(object):
+class Context(object):
     def __init__(self, environ, start_response):
         self.ass2m = Ass2m(environ.get("ASS2M_ROOT", None))
         self._environ = environ
@@ -59,96 +59,99 @@ class Actions(object):
         self.res.status = 200
         self.res.headers['Content-Type'] = 'text/html; charset=UTF-8';
 
+class Actions(object):
+    def __init__(self, environ, start_response):
+        self.ctx = Context(environ, start_response)
 
     def error_notworkingdir(self):
-        self.res.status = 500
-        if self._environ.has_key("ASS2M_ROOT"):
-            self.res.body = self.lookup.get_template('error_notworkingdir.html'). \
-                        render(root=self._environ["ASS2M_ROOT"])
+        self.ctx.res.status = 500
+        if self.ctx.environ.has_key("ASS2M_ROOT"):
+            self.ctx.res.body = self.ctx.lookup.get_template('error_notworkingdir.html'). \
+                        render(root=self.ctx._environ["ASS2M_ROOT"])
         else:
-            self.res.body = self.lookup.get_template('error_norootpath.html'). \
+            self.ctx.res.body = self.ctx.lookup.get_template('error_norootpath.html'). \
                         render()
 
-        return self.res(self._environ, self._start_response)
+        return self.ctx.res(self.ctx._environ, self.ctx._start_response)
 
 
     def answer(self):
-        if not self.ass2m.storage:
+        if not self.ctx.ass2m.storage:
             return self.error_notworkingdir()
 
         self.authenticate()
-        relpath = self.req.path_info
-        fpath = os.path.join(self.ass2m.root, relpath[1:])
-        if self.req.path_info == '/LOGIN':
+        relpath = self.ctx.req.path_info
+        fpath = os.path.join(self.ctx.ass2m.root, relpath[1:])
+        if self.ctx.req.path_info == '/LOGIN':
             return self.login()
 
         if os.path.isdir(fpath):
-            if self.req.path_info[-1] != '/':
-                self.res = HTTPFound(add_slash=True)
-                return self.res(self._environ, self._start_response)
+            if self.ctx.req.path_info[-1] != '/':
+                self.ctx.res = HTTPFound(add_slash=True)
+                return self.ctx.res(self.ctx._environ, self.ctx._start_response)
             if relpath[-1] == '/':
                 # skip the terminated /
                 relpath = os.path.dirname(relpath)
 
         # check perms
-        f = self.ass2m.storage.get_file(relpath)
-        if not self.user.has_perms(f, f.PERM_READ):
-            self.res = HTTPForbidden()
-            return self.res(self._environ, self._start_response)
+        f = self.ctx.ass2m.storage.get_file(relpath)
+        if not self.ctx.user.has_perms(f, f.PERM_READ):
+            self.ctx.res = HTTPForbidden()
+            return self.ctx.res(self.ctx._environ, self.ctx._start_response)
 
         if os.path.isfile(fpath):
             # serve the file, delegate everything to to FileApp
-            self.res = Ass2mFileApp(fpath)
-            return self.res(self._environ, self._start_response)
+            self.ctx.res = Ass2mFileApp(fpath)
+            return self.ctx.res(self.ctx._environ, self.ctx._start_response)
         elif os.path.isdir(fpath):
             return self.listdir(relpath)
         else:
-            self.res = HTTPNotFound()
-            return self.res(self._environ, self._start_response)
+            self.ctx.res = HTTPNotFound()
+            return self.ctx.res(self.ctx._environ, self.ctx._start_response)
 
 
     def listdir(self, relpath):
-        directory = os.path.join(self.ass2m.root, relpath[1:])
+        directory = os.path.join(self.ctx.ass2m.root, relpath[1:])
         dirs = []
         files = []
         for filename in sorted(os.listdir(directory)):
-            f = self.ass2m.storage.get_file(os.path.join(relpath, filename))
-            if not self.user.has_perms(f, f.PERM_LIST):
+            f = self.ctx.ass2m.storage.get_file(os.path.join(relpath, filename))
+            if not self.ctx.user.has_perms(f, f.PERM_LIST):
                 continue
             if os.path.isdir(os.path.join(directory, filename)):
                 dirs.append(filename.decode('utf-8'))
             else:
                 files.append(filename.decode('utf-8'))
 
-        self.res.body = self.lookup.get_template('list.html'). \
+        self.ctx.res.body = self.ctx.lookup.get_template('list.html'). \
                     render(dirs=dirs, files=files, relpath=relpath)
-        return self.res(self._environ, self._start_response)
+        return self.ctx.res(self.ctx._environ, self.ctx._start_response)
 
 
     def authenticate(self):
         signer = AuthCookieSigner(secret=COOKIE_SECRET)
-        cookie = self.req.str_cookies.get('ass2m_auth')
+        cookie = self.ctx.req.str_cookies.get('ass2m_auth')
         user = cookie and signer.auth(cookie)
         if user:
-            self.user = self.ass2m.storage.get_user(user)
+            self.ctx.user = self.ctx.ass2m.storage.get_user(user)
 
 
     def login(self):
         signer = AuthCookieSigner(secret=COOKIE_SECRET)
-        form_user = self.req.str_GET.get('user')
+        form_user = self.ctx.req.str_GET.get('user')
         if form_user:
             # set cookie
             cookie = signer.sign(form_user)
-            self.res.set_cookie('ass2m_auth', cookie)
+            self.ctx.res.set_cookie('ass2m_auth', cookie)
 
-            self.user = self.ass2m.storage.get_user(form_user)
+            self.ctx.user = self.ctx.ass2m.storage.get_user(form_user)
 
-        self.res.body = """<html><body>Current user: %s<br/>
+        self.ctx.res.body = """<html><body>Current user: %s<br/>
                         <form><label>Username</label><input name="user" />
                         <input type="submit" /></form>
-                        </body></html>""" % html_escape(str(self.user))
+                        </body></html>""" % html_escape(str(self.ctx.user))
 
-        return self.res(self._environ, self._start_response)
+        return self.ctx.res(self.ctx._environ, self.ctx._start_response)
 
 
 class Server(object):
