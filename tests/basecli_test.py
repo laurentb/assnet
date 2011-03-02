3a77487d93b8f861955076c51a71605afd1f9d55
Laurent Bachelier <laurent@bachelier.name>
2011-03-02 01:05:22 +0100

    Very basic CLI test

---
+++ tests/basecli_test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from ass2m.cli import CLI
+from tempfile import mkdtemp
+import shutil
+
+class RoutesTest(TestCase):
+    def setUp(self):
+        self.root = mkdtemp(prefix='ass2m_test_root')
+        self.app = CLI(self.root)
+
+    def tearDown(self):
+        if self.root:
+            shutil.rmtree(self.root)
+
+    def test_init(self):
+        assert self.app.main(['ass2m_test', 'init']) in (0, None)
+
+        assert self.app.main(['ass2m_test', 'tree']) in (0, None)
