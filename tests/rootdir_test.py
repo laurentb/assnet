9e3ab389202925b47c7cb358302b0499ae161be2
Laurent Bachelier <laurent@bachelier.name>
2011-03-06 00:35:03 +0100

    Add test for root directory configuration
    Configuration by contructor, and environment.
    Test for the two kind of errors (not a working dir, and no dir
    provided).

---
+++ tests/rootdir_test.py
@@ -0,0 +1,73 @@
+from __future__ import with_statement
+
+from ass2m.ass2m import Ass2m
+from ass2m.server import Server
+
+from unittest import TestCase
+from webtest import TestApp
+
+from tempfile import mkdtemp
+import os
+import os.path
+import shutil
+
+class RootDirTest(TestCase):
+    def setUp(self):
+        self.root = mkdtemp(prefix='ass2m_test_root')
+        with file(os.path.join(self.root, "penguin"), 'a') as f:
+            f.write("HELLO")
+        self.a_wd = os.path.join(self.root, 'a_wd')
+        self.not_a_wd = os.path.join(self.root, 'not_a_wd')
+        os.mkdir(self.a_wd)
+        os.mkdir(self.not_a_wd)
+        ass2m = Ass2m(self.a_wd)
+        ass2m.create(self.a_wd)
+
+    def tearDown(self):
+        if self.root:
+            shutil.rmtree(self.root)
+
+    def test_notWorkingDir(self):
+        server = Server(self.root)
+        app = TestApp(server.process)
+        res = app.get("/", status=500)
+        assert "not an ass2m working directory" in res.body
+        res = app.get("/penguin", status=500)
+        assert "not an ass2m working directory" in res.body
+        res = app.get("/penguins", status=500)
+        assert "not an ass2m working directory" in res.body
+
+    def test_noRootPath(self):
+        server = Server(None)
+        app = TestApp(server.process)
+        res = app.get("/", status=500)
+        assert "No root path was provided" in res.body
+        res = app.get("/penguin", status=500)
+        assert "No root path was provided" in res.body
+        res = app.get("/penguins", status=500)
+        assert "No root path was provided" in res.body
+
+    def test_configByEnv(self):
+        server = Server(None)
+        app = TestApp(server.process)
+        res = app.get("/", status=500, extra_environ={'ASS2M_ROOT': self.root})
+        assert "not an ass2m working directory" in res.body
+        ass2m = Ass2m(self.root)
+        ass2m.create(self.root)
+        res = app.get("/", status=200, extra_environ={'ASS2M_ROOT': self.not_a_wd})
+        assert "penguin" in res.body
+        res = app.get("/penguin", status=200, extra_environ={'ASS2M_ROOT': self.not_a_wd})
+        assert "HELLO" == res.body
+
+        res = app.get("/", status=200, extra_environ={'ASS2M_ROOT': self.a_wd})
+        assert "penguin" not in res.body
+        res = app.get("/penguin", status=404, extra_environ={'ASS2M_ROOT': self.a_wd})
+
+        server = Server(self.a_wd)
+        app = TestApp(server.process)
+        res = app.get("/", status=200)
+        res = app.get("/", status=200, extra_environ={'ASS2M_ROOT': self.root})
+        assert "penguin" in res.body
+        res = app.get("/penguin", status=404)
+        res = app.get("/penguin", status=200, extra_environ={'ASS2M_ROOT': self.root})
+        assert "HELLO" == res.body
