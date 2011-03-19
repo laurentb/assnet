7072389221f7e287328cecc695b93a77d04c69ba
Laurent Bachelier <laurent@bachelier.name>
2011-03-19 11:43:47 +0100

    Test and capture the CLI output

--- tests/basecli_test.py
+++ tests/basecli_test.py
@@ -2,6 +2,9 @@ from unittest import TestCase
 from ass2m.cli import CLI
 from tempfile import mkdtemp
 import shutil
+import sys
+import re
+from StringIO import StringIO
 
 class BaseCLITest(TestCase):
     def setUp(self):
@@ -12,7 +15,26 @@ class BaseCLITest(TestCase):
         if self.root:
             shutil.rmtree(self.root)
 
-    def test_init(self):
-        assert self.app.main(['ass2m_test', 'init']) in (0, None)
+    def beginCapture(self):
+        self.stdout = sys.stdout
+        # begin capture
+        sys.stdout = StringIO()
 
+    def endCapture(self):
+        captured = sys.stdout
+        # end capture
+        sys.stdout = self.stdout
+        self.stdout = None
+        return captured.getvalue()
+
+    def test_init(self):
+        self.beginCapture()
+        assert self.app.main(['ass2m_test', 'init']) in (0, None)
+        output = self.endCapture()
+        assert output.strip() == "Ass2m working directory created."
+
+        self.beginCapture()
         assert self.app.main(['ass2m_test', 'tree']) in (0, None)
+        output = self.endCapture()
+        assert re.match(re.escape(r'/')+r'\s+'+re.escape(r'all(rl-)'), output, re.S)
+        assert re.match(".+"+re.escape(r'/.ass2m/')+r'\s+'+re.escape(r'all(---)'), output, re.S)
