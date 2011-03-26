44cb5465fda0f604ae8e75a5597134abaa4de5a9
Laurent Bachelier <laurent@bachelier.name>
2011-03-26 19:08:47 +0100

    Add a web API
    Very basic, just provides the filenames and types (directory or file).
    The "text" list views adds a trailing / to the directories.
    
    closes #589

---
+++ ass2m/plugins/api.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2011  Romain Bignon, Laurent Bachelier
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+import json
+
+from ass2m.plugin import Plugin
+
+from ass2m.routes import Route
+from ass2m.server import Action
+
+__all__ = ['ApiPlugin']
+
+
+class InfoAction(Action):
+    def answer(self):
+        self.ctx.res.body = json.dumps(self.get_fileinfo(self.ctx.file))
+
+    def get_fileinfo(self, f):
+        fileinfo = dict()
+        if f.isdir():
+            fileinfo['type'] = 'directory'
+        else:
+            fileinfo['type'] = 'file'
+        return fileinfo
+
+
+class JsonListAction(InfoAction):
+    def answer(self):
+        files = dict()
+        for f in self.ctx.iter_files():
+            files[f.get_name()] = self.get_fileinfo(f)
+
+        info = dict(files=files)
+
+        self.ctx.res.body = json.dumps(info)
+
+
+class TextListAction(Action):
+    def answer(self):
+        filenames = []
+        for f in self.ctx.iter_files():
+            if f.isdir():
+                filenames.append(f.get_name()+'/')
+            else:
+                filenames.append(f.get_name())
+
+        self.ctx.res.body = '\n'.join(filenames)
+
+
+class ApiPlugin(Plugin):
+    def init(self):
+        self.register_web_action(
+            Route(object_type = "file", action="info", view="json"),
+            InfoAction)
+
+        self.register_web_action(
+            Route(object_type = "directory", action="info", view="json"),
+            InfoAction)
+
+        self.register_web_action(
+            Route(object_type = "directory", action="list", view="json"),
+            JsonListAction)
+
+        self.register_web_action(
+            Route(object_type = "directory", action="list", view="text"),
+            TextListAction)
---
+++ tests/api_test.py
@@ -0,0 +1,60 @@
+from __future__ import with_statement
+
+from ass2m.ass2m import Ass2m
+from ass2m.server import Server
+
+from unittest import TestCase
+from webtest import TestApp
+
+from tempfile import mkdtemp
+import os
+import shutil
+import json
+
+class ApiTest(TestCase):
+    def setUp(self):
+        self.root = mkdtemp(prefix='ass2m_test_root')
+        ass2m = Ass2m(self.root)
+        ass2m.create(self.root)
+        server = Server(self.root)
+        self.app = TestApp(server.process)
+
+        with open(os.path.join(self.root, 'penguins_are_cute'), 'w') as f:
+            f.write('HELLO')
+        os.mkdir(os.path.join(self.root, 'penguins'))
+        os.mkdir(os.path.join(self.root, 'empty'))
+        with open(os.path.join(self.root, 'penguins', 'gentoo'), 'w') as f:
+            f.write('The best penguin.')
+        with open(os.path.join(self.root, 'penguins', 'emperor'), 'w') as f:
+            f.write('Another kind of penguin. But it is not the best.')
+
+    def tearDown(self):
+        if self.root:
+            shutil.rmtree(self.root)
+
+    def test_listAndDownload(self):
+        res = self.app.get('/?view=json', status=200)
+        data = json.loads(res.body)
+        assert len(data) > 0
+        assert len(data['files']) == 3
+
+        res = self.app.get('/?view=text', status=200)
+        assert len(res.body.split('\n')) == 3
+
+        res = self.app.get('/empty/?view=text', status=200)
+        assert len(res.body) == 0
+
+        res = self.app.get('/empty/?view=json', status=200)
+        data = json.loads(res.body)
+        assert len(data) > 0
+        assert len(data['files']) == 0
+
+        # info on a directory
+        res = self.app.get('/?action=info&view=json', status=200)
+        data = json.loads(res.body)
+        assert data['type'] == 'directory'
+
+        # info on a file
+        res = self.app.get('/penguins/gentoo?action=info&view=json', status=200)
+        data = json.loads(res.body)
+        assert data['type'] == 'file'
