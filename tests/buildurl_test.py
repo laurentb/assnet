5b94733fb2983a923e31ec00fe05ec8614bd56f4
Laurent Bachelier <laurent@bachelier.name>
2011-05-23 23:47:15 +0200

    Add tests for URL building

---
+++ tests/buildurl_test.py
@@ -0,0 +1,35 @@
+from ass2m.storage import Storage
+from ass2m.server import Server
+from ass2m.template import build_url, build_root_url
+from ass2m.filters import quote_url
+
+from unittest import TestCase
+from webtest import TestApp
+
+from tempfile import mkdtemp
+import shutil
+
+
+class BuildURLTest(TestCase):
+    def setUp(self):
+        self.root = mkdtemp(prefix='ass2m_test_root')
+        self.storage = Storage.create(self.root)
+        server = Server(self.root)
+        app = TestApp(server.process)
+        # fill root_url
+        app.get('http://penguin:42/')
+        self.root_url = build_root_url(self.storage)
+
+    def tearDown(self):
+        if self.root:
+            shutil.rmtree(self.root)
+
+    def test_buildRootUrl(self):
+        assert self.root_url.url == 'http://penguin:42/'
+        assert build_root_url(None) is None
+
+    def test_buildSimpleUrl(self):
+        assert quote_url(build_url(self.root_url, self.storage.get_file(''))) \
+                == 'http://penguin:42/'
+        assert quote_url(build_url(self.root_url, self.storage.get_file('/penguin'))) \
+                == 'http://penguin:42/penguin'
