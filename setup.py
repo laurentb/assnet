58ea35ab0b7babca219fe0c21c8a18d9542263b5
Laurent Bachelier <laurent@bachelier.name>
2012-03-10 22:44:33 +0100

    Add long description to package

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ if version_info < (2, 7):
 setup(name="ass2m",
     version='0.1',
     description='The Authenticated Social Storage Made for Mothers project is a web application useful for sharing files (with support for photos galleries, videos, etc.) or organizing events with your friends, removing the obligation of using Facebook or other centralized social networks.',
+    long_description=open('README.rst').read(),
     author='Laurent Bachelier',
     author_email='laurent@bachelier.name',
     url='http://ass2m.org/',
