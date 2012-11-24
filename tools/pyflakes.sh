a59f358f84e388f769fc4a91e9a3d8fe3568b15d
Laurent Bachelier <laurent@bachelier.name>
2012-11-24 20:57:07 +0100

    Better pyflakes.sh using flake8 if possible
    from weboob

--- tools/pyflakes.sh
+++ tools/pyflakes.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash -u
 cd $(dirname $0)
 cd ..
 
-# grep will return 0 only if it finds something, but our script
-# wants to return 0 when it finds nothing!
-pyflakes ass2m | grep -v redefinition && exit 1 || exit 0
+if which flake8 >/dev/null 2>&1; then
+    set -e
+    flake8 --ignore=E,W *.py ass2m tests bin/*
+else
+    # grep will return 0 only if it founds something, but our script
+    # wants to return 0 when it founds nothing!
+    pyflakes *.py ass2m tests bin/* | grep -v redefinition && exit 1 || exit 0
+fi
